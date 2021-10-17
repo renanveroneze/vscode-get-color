9439c54512f75f1fcb5a83462293d3d7d8bc28fb
Renan Veroneze <renan.veroneze@agenciagenia.com.br>
2021-10-17 02:15:17 -0300

    First commit

---
+++ extension.js
@@ -0,0 +1,43 @@
+const vscode = require( 'vscode' )
+const fs = require( 'fs' )
+
+function activate( context ) {
+
+    let disposable = vscode.commands.registerCommand( 'get-color.get-color', async function () {
+
+        const folder = vscode.workspace.workspaceFolders[ 0 ].uri.path
+        const profile = fs.readFileSync( `${folder}/_sources/styles/vars/1.colors.styl` )
+            .toString()
+            .replace( '$colors = {', '' )
+            .replace( '}', '' )
+            .replace( /\s/g, '' )
+            .split( ',' )
+            .filter( Boolean )
+            .map( ( e ) => e.split( ':' ) )
+            .map( ( [ key, val ] ) => ( { "label": `${key}: ${val}`, value: `$colors.${key}` } ) )
+
+        let selected_color = await vscode.window.showQuickPick( profile )
+
+        if( !selected_color ) return
+
+        const editor = vscode.window.activeTextEditor
+
+        if( editor ) {
+            const document = editor.document
+            editor.edit( editBuilder => {
+                editor.selections.forEach( sel => {
+                    const range = sel.isEmpty ? document.getWordRangeAtPosition( sel.start ) || sel : sel
+                    let color = selected_color.value
+                    editBuilder.replace( range, color )
+                } )
+            } )
+        }
+
+    } )
+
+    context.subscriptions.push( disposable )
+}
+
+function deactivate() { }
+
+module.exports = { activate, deactivate }
